90ca02e427bd62dfcec45ed1a354840060c9c84e
leelingyan98 <58385937+leelingyan98@users.noreply.github.com>
2024-06-20 18:24:07 +0800

    fix: Bookmarks page

--- src/pages/Bookmarks/Bookmarks.jsx
+++ src/pages/Bookmarks/Bookmarks.jsx
@@ -1,28 +1,63 @@
-import { useState } from 'react'
-import Post from '../../components/Post/Post'
+import { useState, useEffect } from 'react';
+import Post from '../../components/Post/Post';
+import * as usersAPI from '../../utilities/users-api.js';
+import * as postsAPI from '../../utilities/posts-api.js';
 
 export default function Bookmarks({ user, setUser }) {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    const getAllUserData = async () => {
+      const userData = await usersAPI.findByUserId(user._id);
+      console.log(userData);
+      setUser(userData);
+    }
+    getAllUserData();
+  }, [])
+
+  useEffect(() => {
+    const bookmarks = user.bookmarks;
+
+    const postsArr = [];
+
+    if (bookmarks.length > 0) {
+      for (let i = 0; i < bookmarks.length; i++) {
+        const bookmark = bookmarks[i];
+
+        async function getPost(currentPostId) {
+          const postData = await postsAPI.findByPostId(currentPostId);
+          postsArr.push(postData);
+          setPosts(postsArr);
+        };
+        getPost(bookmark);
+      }
+    }
+  }, [user])
 
   return (
     <>
       <div>
         <p>Bookmarks Page</p>
-        {posts.length > 0 ?
+        {posts ?
         <>
-          {posts.map((post) => (
-            <div className="self-stretch w-full" key={post._id}>
-              <Post
-                user={user}
-                setUser={setUser}
-                postData={post}
-                singlePost={false}
-              />
-            </div>
-          ))}
+        {posts.length > 0 ?
+            <>
+            {posts.map((post) => (
+              <div className="self-stretch w-full" key={post._id}>
+                <Post
+                  user={user}
+                  setUser={setUser}
+                  postData={post}
+                  singlePost={false}
+                />
+              </div>
+            ))}
+          </>
+          :
+          <p>No bookmarks</p>
+          }
         </>
-        :
-        <p>No bookmarks</p>
+        : "Loading..."
       }
       </div>
     </>
